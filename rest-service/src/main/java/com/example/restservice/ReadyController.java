6a08b8d9c9b1b8e14002277e7b1d03d522ac5dd6
Matthew Raspberry <3092450+mraspberry@users.noreply.github.com>
2021-08-11 15:41:15 -0500

    Changed the /ready endpoint to return text

--- rest-service/src/main/java/com/example/restservice/ReadyController.java
+++ rest-service/src/main/java/com/example/restservice/ReadyController.java
@@ -1,6 +1,11 @@
 package com.example.restservice;
 
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +14,9 @@ public class ReadyController {
     private static final String serviceState = "OK";
 
     @GetMapping("/ready")
-    public Ready ready() {
-        return new Ready(this.serviceState);
+    @ResponseBody
+    public String ready(HttpServletResponse response) {
+        response.setContentType(TEXT_PLAIN_VALUE);
+        return new Ready(this.serviceState).getState();
     }
 }
